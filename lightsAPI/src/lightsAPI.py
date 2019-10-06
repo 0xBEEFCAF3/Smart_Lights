1613ceb2c0ce728a2c1da285c4c367b0f35e7907
Armin <armins88@gmail.com>
2019-10-06 12:51:23 -0400

    Removed cached files

--- lightsAPI/src/lightsAPI.py
+++ lightsAPI/src/lightsAPI.py
@@ -2,106 +2,134 @@ import RPi.GPIO as GPIO
 import time
 from math import ceil
 import pigpio
+import time
+from threading import Thread
+
 
 
 class lightsAPI:
-	green_val = 255
-	blue_val = 255
-	red_val = 255
+        green_val = 255
+        blue_val = 255
+        red_val = 255
 
-	DEFAULT_PWM_HZ = 50
-	PIN_TYPE = GPIO.BCM
+        DEFAULT_PWM_HZ = 50
+        PIN_TYPE = GPIO.BCM
+                   
 
-	def __init__(self, red_pin, blue_pin, green_pin):
-		self.red_pin = red_pin
-		self.blue_pin = blue_pin
-		self.green_pin = green_pin
-		self.pi = pigpio.pi()
-		self.init_lights()
+        def __init__(self, red_pin, blue_pin, green_pin):
+                self.red_pin = red_pin
+                self.blue_pin = blue_pin
+                self.green_pin = green_pin
+                self.pi = pigpio.pi()
+                self.return_to_main = True
 
-	def set_green_light(self, green_val):
-		self.pi.set_PWM_dutycycle(self.green_pin, green_val)
+                self.init_lights()
 
-	def set_blue_light(self, blue_val):
+        def set_green_light(self, green_val):
+                self.pi.set_PWM_dutycycle(self.green_pin, green_val)
+
+        def set_blue_light(self, blue_val):
                 self.pi.set_PWM_dutycycle(self.blue_pin, blue_val)
 
-	def set_red_light(self, red_val):
+        def set_red_light(self, red_val):
                 self.pi.set_PWM_dutycycle(self.red_pin, red_val)
 
-	def greenify_lights(self):
-		self.set_green_light(255)
-		self.set_red_light(0)
-		self.set_blue_light(0)
+        def greenify_lights(self):
+                self.set_green_light(255)
+                self.set_red_light(0)
+                self.set_blue_light(0)
 
-	def blueify_lights(self):
+        def blueify_lights(self):
                 self.set_green_light(0)
                 self.set_red_light(0)
                 self.set_blue_light(255)
 
-	def redify_lights(self):
+        def redify_lights(self):
                 self.set_green_light(0)
                 self.set_red_light(255)
                 self.set_blue_light(0)
 
-	def yellowify_lights(self):
+        def yellowify_lights(self):
                 self.set_green_light(255)
                 self.set_red_light(255)
                 self.set_blue_light(0)
 
-	def cyanify_lights(self):
+        def cyanify_lights(self):
                 self.set_green_light(255)
                 self.set_red_light(0)
                 self.set_blue_light(255)
 
-	def magentify_lights(self):
+        def magentify_lights(self):
+                self.rainbow_thread.join();
+
                 self.set_green_light(0)
                 self.set_red_light(255)
                 self.set_blue_light(255)
+        
+        def updateColor(self, color, step):
+            color += step
+        
+            if color > 255:
+                return 255
+            if color < 0:
+                return 0
+                
+            return color
 
-	def map_precent_pwm(self, precent):
-		return 255 - ceil((float(int(precent) - 0) / float(100-0) ) * (255-0))
+        def rainbow_lights(self, steps):
+                self.STEPS = steps
+                r = 255
+                g = 0
+                b = 0
+                while True:
+                    if(self.return_to_main): continue
 
-	def map_rbg_pwm(self, rgb):
+                    if r == 255 and b == 0 and g < 255:
+                            g = self.updateColor(g, self.STEPS)
+                            self.set_green_light(g)
+                    
+                    elif g == 255 and b == 0 and r > 0:
+                            r = self.updateColor(r, -self.STEPS)
+                            self.set_red_light(r)
+                    
+                    elif r == 0 and g == 255 and b < 255:
+                            b = self.updateColor(b, self.STEPS)
+                            self.set_blue_light(b)
+                    
+                    elif r == 0 and b == 255 and g > 0:
+                            g = self.updateColor(g, -self.STEPS)
+                            self.set_green_light(g)
+                    
+                    elif g == 0 and b == 255 and r < 255:
+                            r = self.updateColor(r, self.STEPS)
+                            self.set_red_light(r)
+                    
+                    elif r == 255 and g == 0 and b > 0:
+                            b = self.updateColor(b, -self.STEPS)
+                            self.set_blue_light(b)
+
+                
+        
+        def map_precent_pwm(self, precent):
+                return 255 - ceil((float(int(precent) - 0) / float(100-0) ) * (255-0))
+
+        def map_rbg_pwm(self, rgb):
                 return  100 - ceil((float(int(rgb) - 0) / float(255-0) ) * (100-0))
 
 
-	def init_lights(self):
-#                self.clean_up()
-#                GPIO.setwarnings(False) 
-#                GPIO.setmode(self.PIN_TYPE)
+        def init_lights(self):
                 print("In init lights", self.red_pin, self.blue_pin, self.green_pin)
                 self.pi.set_PWM_dutycycle(self.red_pin, 255)
                 self.pi.set_PWM_dutycycle(self.blue_pin, 255)
                 self.pi.set_PWM_dutycycle(self.green_pin, 255)
-                ##Set up pins as output
-#                GPIO.setup(self.red_pin, GPIO.OUT)
-#                GPIO.setup(self.blue_pin, GPIO.OUT)
-#                GPIO.setup(self.green_pin, GPIO.OUT)
 
-#                self.pwm_red = GPIO.PWM(self.red_pin, self.DEFAULT_PWM_HZ)
-#                self.pwm_red.start(self.red_val)
-#                self.pwm_blue = GPIO.PWM(self.blue_pin, self.DEFAULT_PWM_HZ)
-#                self.pwm_blue.start(self.blue_val)
-#                self.pwm_green = GPIO.PWM(self.green_pin, self.DEFAULT_PWM_HZ)
-#                self.pwm_green.start(self.green_val)
+        def clean_up(self):
+                self.pi.set_PWM_dutycycle(self.red_pin, 0)
+                self.pi.set_PWM_dutycycle(self.blue_pin, 0)
+                self.pi.set_PWM_dutycycle(self.green_pin, 0)
 
-	def clean_up(self):
-		self.pi.set_PWM_dutycycle(self.red_pin, 0)
-		self.pi.set_PWM_dutycycle(self.blue_pin, 0)
-		self.pi.set_PWM_dutycycle(self.green_pin, 0)
+#obj = lightsAPI(17, 22, 24)
+#obj.rainbow_lights(0.05)
 
-#if __name__ == "__main__":
-#	print("Ready to go!")
-#	lightsAPI = lightsAPI(17,22,24)
-#	lightsAPI.clean_up()
-#	lightsAPI.magentify_lights()
-#	time.sleep(5)
-#	lightsAPI.alternative_rainbow()
-
-#	lightsAPI.set_all_colors(0,0,0)
-	#lightsAPI.set_green_light(20)
-
-#	time.sleep(5)
-#	lightsAPI.alternative_rainbow()
-#	lightsAPI.clean_up()
+#time.sleep(5000)
 
